747a053c44c16ad558032741dae5c34a09099b1a
Csheet <109898681+idonotexist-0@users.noreply.github.com>
2023-01-11 01:53:30 +0630

    Add files via upload

--- function.js
+++ function.js
@@ -1,16 +1,19 @@
 // Get the header element
 const header = document.querySelector('#header');
+const header_a = document.querySelector('#header_a');
 
 // Listen for the scroll event on the window object
 window.addEventListener('scroll', function() {
   // Get the current scroll position
   const scrollPos = window.scrollY;
 
-  // If the scroll position is greater than 50, set the header's background color to #333 with 80% opacity
-  if (scrollPos > 50) {
+  // If the scroll position is greater than 56, set the header's background color to #333 with 80% opacity
+  if (scrollPos > 56) {
     header.style.backgroundColor = 'rgba(22, 27, 34, 0.8)';
+    header_a.style.backgroundColor = 'rgba(48, 57, 68, 0.4)';
   } else {
     // Otherwise, set the header's background color to transparent
     header.style.backgroundColor = 'rgba(22, 27, 34, 1)';
+    header_a.style.backgroundColor = 'rgba(48, 57, 68, 1)';
   }
 });
